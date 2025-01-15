6f97d3352cae74b6bb6869c888b11caa9bbb08c9
jack_wr <jack_wr@hotmail.co.uk>
2025-01-15 15:06:36 +0000

    Added healthcheck controller

---
+++ JackOfAllCodes.Web/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JackOfAllCodes.Web.Controllers
+{
+    public class HealthController : Controller
+    {
+        public ActionResult Check()
+        {
+            return Content("Health Check OK");
+        }
+    }
+}
